e5bc194f8ce9ac32d383b0332bc13caec0e45e0f
Marios Andreopoulos <opensource@andmarios.com>
2015-09-23 23:46:18 +0300

    add example

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# cryptographer (golang package) #
-
+# cryptographer (golang package)
 
 Cryptographer provides a simple way to encrypt or decrypt a message using NaCl secret key cryptography.
 Optionally it can compress the data before encrypting them.
@@ -7,6 +6,46 @@ Optionally it can compress the data before encrypting them.
 I use it for symmetric-key encryption schemes. Depending on your usage it may or may not be a safe option.
 I do not claim any expertise in cryptography.
 
-To use:
+## Usage
 
     import "github.com/andmarios/cryptographer"
+
+## Example
+
+```go
+package main
+
+import (
+	"github.com/andmarios/cryptographer"
+	"log"
+)
+
+func main() {
+	// Create a cryptographer instance with "qwerty" key and no compression.
+	c, err := cryptographer.New("qwerty", "qwertyuiopasdfghjklzxcvbnm123456", false)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Message to be encrypted
+	msg := []byte("Hello World")
+
+	// Encrypt message
+	encMsg, err := c.Encrypt(msg)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// Decrypt message
+	decMsg, err := c.Decrypt(encMsg)
+	if err != nil {
+		log.Fatalln("Could not decrypt message")
+	}
+	log.Printf("Decrypted message is '%s'.\n", decMsg)
+}
+```
+
+## License
+
+You can find more information inside the `LICENSE` file. In short this software uses
+a BSD 3-Clause license.
---
+++ example/example.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/andmarios/cryptographer"
+	"log"
+)
+
+func main() {
+	// Create a cryptographer instance with "qwerty" key and no compression.
+	c, err := cryptographer.New("qwerty", "qwertyuiopasdfghjklzxcvbnm123456", false)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// A 1 kilobyte message
+	msg1K := []byte(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam egestas mi ut nisl lobortis rhoncus. Cras et tempus elit. Maecenas dictum viverra pretium. Integer porta felis lacus, ac maximus risus volutpat vel. Vivamus fermentum vitae turpis vitae lobortis. Quisque dictum, nunc eget blandit porttitor, risus nisl aliquam ante, vel congue nibh metus at velit. Suspendisse potenti. Pellentesque quis ipsum vitae tortor condimentum malesuada id vitae enim. Mauris mattis elit quis nibh venenatis, ut finibus mauris pulvinar. Duis facilisis tellus nec laoreet cursus. Sed mattis condimentum condimentum. Aenean a leo vel urna pharetra scelerisque tempor accumsan augue. Sed vel ante id turpis tempus ornare. Quisque lobortis enim auctor ipsum lacinia, et bibendum tortor pellentesque.
+
+Vivamus ut elit nec arcu congue malesuada nec eget enim. Aliquam erat volutpat. Phasellus auctor consequat est et hendrerit. Nullam vitae odio ac nisi blandit viverra. Curabitur consequat urna quis ante molestie viverra. Donec malesuada amet.`)
+
+	// Encrypt message
+	encMsg, err := c.Encrypt(msg1K)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Printf("Uncompressed encrypted 1K message size is %d bytes due to overhead.\n", len(encMsg))
+
+	// Decrypt message
+	decMsg, err := c.Decrypt(encMsg)
+	if err != nil {
+		log.Fatalln("Could not decrypt message")
+	}
+	log.Println("Decrypted message is:\n" + string(decMsg))
+
+	// Create a cryptographer instance with "qwerty" key and compression.
+	c, err = cryptographer.New("qwerty", "qwertyuiopasdfghjklzxcvbnm123456", true)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Encrypt message
+	encMsg, err = c.Encrypt(msg1K)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Printf("Compressed encrypted 1K message size is %d bytes despite overhead.\n", len(encMsg))
+}
