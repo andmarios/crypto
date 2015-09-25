d8e65f0f4c6edfb658c045fe438e361b222cdf12
Marios Andreopoulos <opensource@andmarios.com>
2015-09-26 01:26:12 +0300

    example formatting

--- example_test.go
+++ example_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func ExampleCryptographer_Encrypt() {
-	// Create a cryptographer instance with "qwerty" key and no compression.
-	c, err := cryptographer.New("qwerty", "qwertyuiopasdfghjklzxcvbnm123456", false)
+	// Create a cryptographer instance with "password" key and no compression.
+	c, err := cryptographer.New("password", "qwertyuiopasdfghjklzxcvbnm123456", false)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -27,13 +27,16 @@ func ExampleCryptographer_Encrypt() {
 }
 
 func ExampleCryptographer_Decrypt() {
-	// Create a cryptographer instance with "qwerty" key and no compression.
-	c, err := cryptographer.New("qwerty", "qwertyuiopasdfghjklzxcvbnm123456", false)
+	// Create a cryptographer instance with "password" key and no compression.
+	c, err := cryptographer.New("password", "qwertyuiopasdfghjklzxcvbnm123456", false)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	encryptedMsg := []byte{173, 25, 228, 30, 140, 142, 245, 186, 114, 121, 122, 174, 252, 221, 52, 154, 25, 3, 119, 18, 222, 148, 0, 80, 32, 190, 67, 167, 34, 69, 7, 38, 5, 139, 165, 56, 32, 131, 13, 185, 10, 43, 148, 3, 81, 181, 31, 155, 48, 209, 169}
+	encryptedMsg := []byte{173, 25, 228, 30, 140, 142, 245, 186, 114, 121,
+		122, 174, 252, 221, 52, 154, 25, 3, 119, 18, 222, 148, 0, 80,
+		32, 190, 67, 167, 34, 69, 7, 38, 5, 139, 165, 56, 32, 131, 13,
+		185, 10, 43, 148, 3, 81, 181, 31, 155, 48, 209, 169}
 
 	// Decrypt message
 	decryptedMsg, err := c.Decrypt(encryptedMsg)
